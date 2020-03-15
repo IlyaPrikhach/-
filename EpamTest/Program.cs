fed2e7ab91c0791f147a7b4a26e101c456d71235
IlyaPrikhach <61990763+IlyaPrikhach@users.noreply.github.com>
2020-03-15 19:29:04 +0300

    Add files via upload

---
+++ EpamTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpamTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x=0;
+            Console.WriteLine("Введите число");
+            try
+            {
+                string строкаВвода = Console.ReadLine();
+                string[] аргумент = строкаВвода.Replace('.',',').Split(' ');
+                x = Convert.ToInt16(аргумент[0]);
+            }
+            catch
+            {
+                Console.WriteLine("неверные данные");
+                Main(args);
+            }
+            double y = Math.Pow(x, 2);
+            Console.WriteLine("Ваше число в квадрате");
+            Console.WriteLine(y);
+            Console.ReadLine();
+        }
+    }
+}
